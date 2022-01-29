f7d2a2f9863d1285742da5c73ae123a9110e82c6
kjopnfer <gaypal0805@gmail.com>
2022-01-29 14:29:19 -0500

    Three link, two joint robot

--- generate.py
+++ generate.py
@@ -23,7 +23,7 @@ def Create_Robot():
     width = 1
     height = 1
 
-    pyrosim.Send_Cube(name="Link0", pos=[0, 0, 0.5], size=[length, width, height])
+    '''pyrosim.Send_Cube(name="Link0", pos=[0, 0, 0.5], size=[length, width, height])
 
     pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[0, 0, 1])
 
@@ -51,8 +51,17 @@ def Create_Robot():
 
     pyrosim.Send_Joint(name="Link6_Link7", parent="Link6", child="Link7", type="revolute", position=[0, 0, -0.5])
 
-    pyrosim.Send_Cube(name="Link7", pos=[0, 0, -0.5], size=[length, width, height])
+    pyrosim.Send_Cube(name="Link7", pos=[0, 0, -0.5], size=[length, width, height])'''
 
+    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[length, width, height])
+
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1, 0, 1])
+
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[length, width, height])
+
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[2, 0, 1])
+
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[length, width, height])
     pyrosim.End()
 
 
